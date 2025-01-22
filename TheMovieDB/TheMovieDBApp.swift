9b6c8bec590759697c4eab6d1f6d1bcba36ff539
ZYP <zayarphyo16.mm@gmail.com>
2025-01-23 01:09:21 +0800

    Initial Commit

---
+++ TheMovieDB/TheMovieDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMovieDBApp.swift
+//  TheMovieDB
+//
+//  Created by Zay Yar Phyo on 23/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct TheMovieDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
